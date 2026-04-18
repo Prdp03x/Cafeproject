c7afbad5d0aa94da261330971dd5abb13f392961
Prdp03x <115916611+Prdp03x@users.noreply.github.com>
2026-04-18 18:40:22 +0530

    Dashboard changes

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  //  baseURL: "https://selfserviceorder-1.onrender.com/api"
-  baseURL: "http://localhost:5000/api"
+   baseURL: "https://cafeproject-nn33.onrender.com/api"
+  // baseURL: "http://localhost:5000/api"
 });
 
 export const signup = (data) => API.post("/auth/signup", data);
